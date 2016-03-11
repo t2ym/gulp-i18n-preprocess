ce845da72651b2ce4a7606f701c1180ea2fa81f5
Tetsuya Mori <t2y3141592@gmail.com>
2016-03-11 10:38:04 +0900

    test: Use test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 ./node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha --report lcov -- -R spec
-cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
+if [ "${TRAVIS_BRANCH}" != "" ]; then cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js; fi
+
