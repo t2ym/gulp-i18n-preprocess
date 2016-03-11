e0c087a05a5190e5f8245db3d8d7405adccefad6
Tetsuya Mori <t2y3141592@gmail.com>
2016-03-11 10:44:49 +0900

    test: Revert to the initial scheme

--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-./node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha --report lcov -- -R spec
-if [ "${TRAVIS_BRANCH}" != "" ]; then cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js; fi
-
